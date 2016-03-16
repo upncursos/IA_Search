2a4b7b2dbca864d817644668e5baf6e3f90df64d
upncursos <upncursos@gmail.com>
2016-03-16 11:10:42 -0500

    còdigo de clase

--- generar_estados_siguientes.m
+++ generar_estados_siguientes.m
@@ -3,12 +3,28 @@ function estados_siguientes = generar_estados_siguientes( estado_actual )
       estados_siguientes=[];
       
       
+  %%%%% se va un misionero    
+      if estado_actual.bote==-1
       
-      for estado_siguiente = estados_siguientes_originales
-          estado_siguiente_clonado = clonar(estado_siguiente); 
-          estado_siguiente_clonado.agregar_antecesor(estado_actual);
-          estados_siguientes = [estados_siguientes clonar(estado_siguiente_clonado)];
+      nuevo=clonar(estado_actual);
+      nuevo.misioneros_orilla1 = nuevo.misioneros_orilla1 -1;
+      nuevo.misioneros_orilla2 = nuevo.misioneros_orilla2 +1;
+      if es_estado_posible(nuevo)
+          estados_siguientes=[estados_siguientes nuevo];
       end
+          
+      else
+         
+      nuevo=clonar(estado_actual);
+      nuevo.misioneros_orilla2 = nuevo.misioneros_orilla2 -1;
+      nuevo.misioneros_orilla1 = nuevo.misioneros_orilla1 +1;   
+      if es_estado_posible(nuevo)
+       estados_siguientes=[estados_siguientes nuevo];
+      end 
+           
+      end
+      
+      
       
 end
 
---
+++ prueba.m
@@ -0,0 +1,11 @@
+clc;
+e = Estado();
+e.canibales_orilla1=3;
+e.misioneros_orilla1=3;
+e.canibales_orilla2=0;
+e.misioneros_orilla2=0;
+disp('Estado actual');
+mostrar_informacion(e);
+disp('estados siguientes');
+s = generar_estados_siguientes(e);
+mostrar_informacion_estados(s);
