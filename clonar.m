8ce88f9e23d8e8976915734b2cb1b83bd66543f1
upncursos <upncursos@gmail.com>
2016-03-16 11:13:45 -0500

    archivo clonar corregido para versiòn matlab 2007b

--- clonar.m
+++ clonar.m
@@ -1,7 +1,7 @@
 function nuevo = clonar(objeto)
      nuevo = feval(class(objeto));
-     propiedades = properties(objeto);
+     propiedades = fieldnames(objeto);
      for i = 1:length(propiedades)
-        nuevo.(propiedades{i})=objeto.(propiedades{i});
+        nuevo.(char(propiedades(i)))=objeto.(char(propiedades(i)));
      end
 end
